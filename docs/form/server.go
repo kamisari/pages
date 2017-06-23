924aa288ecc7f9e8082d065aa2128327bc2a7b03
kamisari <kamisari10yonaga@gmail.com>
2017-06-23 12:32:32 +0900

    add textarea  Changes to be committed: 	modified:   form.html 	modified:   server.go

--- docs/form/server.go
+++ docs/form/server.go
@@ -9,7 +9,10 @@ func main() {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		log.Println("req.URL: ", r.RequestURI)
 		log.Println("form[key]: ", r.FormValue("key"))
-		log.Printf("form[key2]: %s\n\n", r.FormValue("key2"))
+		log.Println("form[key2]: ", r.FormValue("key2"))
+		if s := r.FormValue("text"); s != "" {
+			log.Printf("from [text]:%s\n", s)
+		}
 	}
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
